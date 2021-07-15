697962d136cf9f49e5a39c2b4dfd9d7ae19a6ce4
micooper9041 <micooper9041@gmail.com>
2021-07-15 02:10:31 -0400

    Edited hero banner, changed background image to free code related image

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,13 +9,6 @@ function Nav({ handleClick }) {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
-              <div className="flex-shrink-0">
-                <img
-                  className="h-8 w-8"
-                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                  alt="Workflow"
-                />
-              </div>
               <div className="hidden md:block">
                 <div className="ml-10 flex items-baseline space-x-4">
                   <button
