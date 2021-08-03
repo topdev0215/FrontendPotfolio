7d228bcde8acd9a18a6efa95a624617a2b99cab1
micooper9041 <micooper9041@gmail.com>
2021-08-03 16:37:18 -0400

    Made it so that resume iframe is invisible on small screens due to formatting issues

--- src/components/ResumeDownload.js
+++ src/components/ResumeDownload.js
@@ -7,7 +7,7 @@ const ResumeDownload = () => {
     <div className="h-screen pt-5 flex items-center flex-col text-gray-400 bg-gray-900">
       <iframe
         title="Resume"
-        className="h-full w-1/2"
+        className="invisible lg:visible lg:w-1/2 lg:h-full sm:invisible"
         src="https://docs.google.com/document/d/e/2PACX-1vS8ejoqo5OJqkUf2p3U2u3jPoiwNJypI46e-NCr0SD8Hspb7C5_OWWDV-y_DCDvtk8No-LMYu2QbsUk/pub?embedded=true"
       ></iframe>
       <div className="flex justify-center">
