15f122c668c24ea670433e7f1e1c40e9b098350b
micooper9041 <micooper9041@gmail.com>
2021-08-03 12:20:58 -0400

    Made a few changes to Resume page so resume displays and has a link to downloadable version.

--- src/components/ResumeDownload.js
+++ src/components/ResumeDownload.js
@@ -3,19 +3,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFile } from "@fortawesome/free-regular-svg-icons";
 
 const ResumeDownload = () => {
-  const element = <FontAwesomeIcon icon={faFile} size="6x" color="white" />;
   return (
-    <div className="h-screen pt-5 flex flex-col text-gray-400 bg-gray-900">
-      <a
-        href="../assets/docs/resume.pdf"
-        className="flex justify-center"
-        download
-      >
-        {element}
-      </a>
+    <div className="h-screen pt-5 flex items-center flex-col text-gray-400 bg-gray-900">
+      <iframe
+        title="Resume"
+        className="h-full w-2/3"
+        src="https://docs.google.com/document/d/e/2PACX-1vS8ejoqo5OJqkUf2p3U2u3jPoiwNJypI46e-NCr0SD8Hspb7C5_OWWDV-y_DCDvtk8No-LMYu2QbsUk/pub?embedded=true"
+      ></iframe>
       <div className="flex justify-center">
         <br />
-        <p className="text-xl"> Click to Download</p>
+        <p
+          onClick={() =>
+            window.open(
+              "https://drive.google.com/file/d/19HjOVGlLAh4OfPXCpzaBUYcC90a6IpfW/view?usp=sharing"
+            )
+          }
+          className="cursor-pointer text-xl"
+        >
+          Click for Downloadable Version
+        </p>
       </div>
     </div>
   );
