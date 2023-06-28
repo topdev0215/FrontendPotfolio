7128a7bac21b81ef7255e348416aa2bca4108b96
micooper9041 <micooper9041@gmail.com>
2023-06-28 00:16:15 -0400

    Changed resume download information to reflect latest resume version

--- src/components/ResumeDownload.js
+++ src/components/ResumeDownload.js
@@ -5,17 +5,13 @@ import { faFile } from '@fortawesome/free-regular-svg-icons';
 const ResumeDownload = () => {
   return (
     <div className="h-screen pt-5 flex items-center flex-col text-gray-400 bg-gray-900">
-      <iframe
-        title="Resume"
-        className="invisible lg:visible lg:w-1/2 lg:h-full sm:invisible"
-        src="https://docs.google.com/document/d/e/2PACX-1vQNHvgcVg-oJmFtBoFCjCt4F0k56LlRp5c4ZU7oERyn78jFRRnnU3UVWkGr-jgpmhAEzJmlTS9fwVzH/pub?embedded=true"
-      ></iframe>
+      <iframe  className="invisible lg:visible lg:w-1/2 lg:h-full sm:invisible" src="https://drive.google.com/file/d/1wzLlVJw_FhOVSxCEmFk3mMMVUfeO8jk6/preview"></iframe>
       <div className="flex justify-center">
         <br />
         <p
           onClick={() =>
             window.open(
-              'https://drive.google.com/file/d/19HjOVGlLAh4OfPXCpzaBUYcC90a6IpfW/view?usp=sharing'
+              'https://drive.google.com/file/d/1wzLlVJw_FhOVSxCEmFk3mMMVUfeO8jk6/view?usp=sharing'
             )
           }
           className="cursor-pointer text-xl"
