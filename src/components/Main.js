5099c574153137a4b10b75ba374837cd8430b6da
micooper9041 <micooper9041@gmail.com>
2023-06-28 02:20:44 -0400

    Fixed image on main page so it doesn't look weird

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,11 +4,11 @@ const Main = () => {
   return (
     <div className="h-full relative bg-white overflow-hidden">
       <div className="max-w-7xl mx-auto">
-        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+        <div className="relative z-10 pb-8 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
           <svg
             className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
             fill="currentColor"
-            viewBox="0 0 100 100"
+            viewBox="0 0 0 0"
             preserveAspectRatio="none"
             aria-hidden="true"
           >
@@ -65,7 +65,7 @@ const Main = () => {
         </div>
         <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
           <img
-            className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-auto"
+            className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-auto xl:h-full"
             src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2700&q=80"
             alt=""
           />
